9852d80c75d34cd03b3812ee49c9b8ad3ea6cd47
local <local02070@gmail.com>
2023-06-20 00:33:53 +0900

    admin 패키지 추가

--- src/main/java/com/example/project_machimo/admin/adminDao/AdminDao.java
+++ src/main/java/com/example/project_machimo/admin/adminDao/AdminDao.java
@@ -1,4 +1,9 @@
 package com.example.project_machimo.admin.adminDao;
 
+import com.example.project_machimo.admin.adminDto.AdminDto;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.ArrayList;
+
+@Mapper
 public interface AdminDao {
 }
