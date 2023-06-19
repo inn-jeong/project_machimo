ac6bb0421f647bdf0cbd95bd3010991103b6401c
inn-jeong <jinpal429@gmail.com>
2023-06-19 18:24:16 +0900

    Initial commit

---
+++ src/main/java/com/example/project_machimo/ProjectMachimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_machimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMachimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMachimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_machimo/ProjectMachimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_machimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMachimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
