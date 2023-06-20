353a555dc768684662c57aab79e24eb415929333
jjjooosss <suuuu3030@gmail.com>
2023-06-20 15:23:04 +0900

    Changes

---
+++ src/main/java/com/example/project_machimo/review/dao/ReviewDao.java
@@ -0,0 +1,16 @@
+package com.example.project_machimo.review.dao;
+
+import com.example.project_machimo.review.dto.ReviewDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+@Mapper
+public interface ReviewDao {
+    ArrayList<ReviewDto> list();
+    void write(HashMap<String,String> param);
+
+    ReviewDto contentView(HashMap<String,String> param);
+    void modify(HashMap<String,String> param);
+    void delete(HashMap<String,String> param);
+}
