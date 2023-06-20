9aa296eb012cfee1d6eb1b4c20f409e671d7be47
jjjooosss <suuuu3030@gmail.com>
2023-06-20 15:21:35 +0900

    Changes

--- src/main/java/com/example/project_machimo/review/dao/Reviewdao.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_machimo.review.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class Reviewdao {
-}
--- src/main/java/com/example/project_machimo/review/service/ReviewService.java
+++ src/main/java/com/example/project_machimo/review/service/ReviewService.java
@@ -1,8 +1,18 @@
 package com.example.project_machimo.review.service;
 
+import com.example.project_machimo.review.dto.ReviewDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public interface ReviewService {
+    ArrayList<ReviewDto> list();
+    void write(HashMap<String,String> param);
+
+    ReviewDto contentView(HashMap<String,String> param);
+    void modify(HashMap<String,String> param);
+    void delete(HashMap<String,String> param);
 }
