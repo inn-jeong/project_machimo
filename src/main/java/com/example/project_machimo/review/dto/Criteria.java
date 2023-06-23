7b070653e90f7a464a87d39864f6bfda91bedc57
jjjooosss <suuuu3030@gmail.com>
2023-06-23 10:39:45 +0900

    fix:paging

--- src/main/java/com/example/project_machimo/review/dto/Criteria.java
+++ src/main/java/com/example/project_machimo/review/dto/Criteria.java
@@ -3,9 +3,11 @@ package com.example.project_machimo.review.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class Criteria {
     private int pageNum;//페이지 번호
