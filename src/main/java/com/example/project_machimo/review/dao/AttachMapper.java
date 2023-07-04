9973410736ace81c3b4e82796d1fbad18b8dfd5e
jjjooosss <suuuu3030@gmail.com>
2023-07-04 15:26:10 +0900

    이미지수정안된상태,빌드오류해결

--- src/main/java/com/example/project_machimo/review/dao/AttachMapper.java
+++ src/main/java/com/example/project_machimo/review/dao/AttachMapper.java
@@ -1,13 +1,13 @@
-//package com.example.project_machimo.review.dao;
-//
-//import com.example.project_machimo.review.dto.AttachImageVO;
-//import org.apache.ibatis.annotations.Mapper;
-//
-//import java.util.List;
-//
-//@Mapper
-//public interface AttachMapper {
-//
-//    /* 이미지 데이터 반환 */
-//    public List<AttachImageVO> getAttachList(int reviewId);
-//}
+package com.example.project_machimo.review.dao;
+
+import com.example.project_machimo.review.dto.AttachImageVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AttachMapper {
+
+    /* 이미지 데이터 반환 */
+    public List<AttachImageVO> getAttachList(int reviewId);
+}
--- src/main/java/com/example/project_machimo/review/service/AttachImageService.java
+++ src/main/java/com/example/project_machimo/review/service/AttachImageService.java
@@ -1,25 +1,25 @@
-//package com.example.project_machimo.review.service;
-//
-//import com.example.project_machimo.review.dto.AttachImageVO;
-//import com.example.project_machimo.review.dao.AttachMapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.ibatis.session.SqlSession;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@Slf4j
-//public class AttachImageService {
-//
-//    @Autowired
-//    private SqlSession sqlSession;
-//
-//    public List<AttachImageVO> getAttachList(int reviewId){
-//        AttachMapper mapper = sqlSession.getMapper(AttachMapper.class);
-//        log.info("getAttachList.........");
-//        return mapper.getAttachList(reviewId);
-//    };
-//
-//}
+package com.example.project_machimo.review.service;
+
+import com.example.project_machimo.review.dto.AttachImageVO;
+import com.example.project_machimo.review.dao.AttachMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class AttachImageService {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    public List<AttachImageVO> getAttachList(int reviewId){
+        AttachMapper mapper = sqlSession.getMapper(AttachMapper.class);
+        log.info("getAttachList.........");
+        return mapper.getAttachList(reviewId);
+    };
+
+}
